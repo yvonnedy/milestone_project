af9820d244045a12c9ae5ee12c7c4d46c39049a1
yvonnedy <kiiimberly@126.com>
2017-12-10 13:14:39 -0800

    updated the script

--- src/analyze_data.R
+++ src/analyze_data.R
@@ -2,7 +2,8 @@
 # analyze_data.R
 # Ying Dong, Dec 2017
 #
-# This script filters survived people in each passager class.
+# This script filters survived people in each passager class 
+# and write the result to the results directory in CSV. 
 # This script takes an input filename and an output filename as the variable arguments. 
 # 
 # Usage: Rscript src/analyze_data.R data/Titanic.csv results/filter_survived.csv
