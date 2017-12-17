ecdde2552c8376e7c993e9a750cfa1b7a9a34a90
yvonnedy <kiiimberly@126.com>
2017-12-16 17:45:39 -0800

    fix the link in the usage part

--- src/download_data.R
+++ src/download_data.R
@@ -5,7 +5,7 @@
 # This script glimpses the titanic data and saves it locally.
 # This script takes a URL and output filename as the variable arguments.
 #
-# Usage: Rscript src/download_data.R https://raw.github.ubc.ca/ubc-mds-2017/datasets/master/data/titanic.csv?token=AAADoDScWJc96cKeqViGL4nHskjD5Mu3ks5aLaO8wA%3D%3D data/Titanic.csv
+# Usage: Rscript src/download_data.R "https://raw.github.ubc.ca/ubc-mds-2017/datasets/master/data/titanic.csv?token=AAADqC0ANzUKk2ZCPXF4Sb13ucDojAHxks5aNpHbwA%3D%3D" data/Titanic.csv
 
 # read in command line arguments
 args <- commandArgs(trailingOnly = TRUE)
@@ -13,7 +13,7 @@ URL <- args[1]
 output_file <- args[2]
 
 # read in data
-titanic <- read.csv2(URL, header=TRUE, sep=",")
+titanic <- read.csv(URL)
     
 # glimpse first 6 rows of data
 head(titanic)
